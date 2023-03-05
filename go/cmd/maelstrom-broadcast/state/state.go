4721e8837459dd09a03eb39203e3d43859060125
Michael DeWitt <michael@dewitt.io>
2023-03-04 22:26:54 -0800

    handle race conditions where the store may be read as it is reallocated by an append

--- go/cmd/maelstrom-broadcast/state/state.go
+++ go/cmd/maelstrom-broadcast/state/state.go
@@ -1,9 +1,16 @@
 package state
 
+import "log"
+
+type event interface {
+	// event is just a marker interface
+	event()
+}
+
 // State appends values to a store via a serialized interface
 type State struct {
 	store   []int32
-	input   chan int32
+	input   chan event
 	closing chan struct{}
 	closed  chan struct{}
 }
@@ -12,7 +19,7 @@ type State struct {
 func NewState() *State {
 	s := &State{
 		store:   make([]int32, 0),
-		input:   make(chan int32),
+		input:   make(chan event),
 		closing: make(chan struct{}),
 		closed:  make(chan struct{}),
 	}
@@ -27,26 +34,47 @@ func (s *State) Close() {
 	close(s.input)
 }
 
+// Append adds a value to the store
+func (s *State) Append(n int32) {
+	s.input <- &appendEvent{n}
+}
+
+// Read returns all stored values
+func (s *State) Read() []int32 {
+	output := make(chan []int32)
+	s.input <- &readEvent{output}
+	return <-output
+}
+
+type appendEvent struct {
+	value int32
+}
+
+func (a *appendEvent) event() {}
+
+type readEvent struct {
+	output chan<- []int32
+}
+
+func (r *readEvent) event() {}
+
 func (s *State) loop() {
 	for {
 		select {
 		case <-s.closing:
 			close(s.closed)
 			return
-		case n := <-s.input:
-			s.store = append(s.store, n)
+		case ev := <-s.input:
+			switch ev.(type) {
+			case *appendEvent:
+				s.store = append(s.store, ev.(*appendEvent).value)
+			case *readEvent:
+				read := make([]int32, len(s.store))
+				copy(read, s.store)
+				ev.(*readEvent).output <- read
+			default:
+				log.Panicf("invalid event %v", ev)
+			}
 		}
 	}
-}
-
-// Append adds a value to the store
-func (s *State) Append(n int32) {
-	s.input <- n
-}
-
-// Read returns all stored values
-func (s *State) Read() []int32 {
-	read := make([]int32, len(s.store))
-	copy(read, s.store)
-	return read
 }
