1425d75b511df61f7eb66eb6e77842885b0f8c27
Michael DeWitt <michael@dewitt.io>
2023-04-07 21:50:38 -0700

    reorder and add some figlets

--- rust/broadcast/src/main.rs
+++ rust/broadcast/src/main.rs
@@ -28,6 +28,109 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Clone, Default)]
+pub struct State {
+    messages: Arc<RwLock<Vec<u64>>>,
+    topology: Arc<RwLock<HashMap<NodeId, Vec<NodeId>>>>,
+
+    next_id: Arc<AtomicU64>,
+    pending: Arc<RwLock<HashMap<MsgId, Message<Replicate>>>>,
+}
+
+//  _                    _
+// | |_ ___  _ __   ___ | | ___   __ _ _   _
+// | __/ _ \| '_ \ / _ \| |/ _ \ / _` | | | |
+// | || (_) | |_) | (_) | | (_) | (_| | |_| |
+//  \__\___/| .__/ \___/|_|\___/ \__, |\__, |
+//          |_|                  |___/ |___/
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(tag = "type", rename = "topology")]
+pub struct Topology {
+    #[serde(flatten)]
+    headers: Headers,
+    topology: HashMap<NodeId, Vec<NodeId>>,
+}
+
+#[derive(Debug, Default, Serialize)]
+#[serde(tag = "type", rename = "topology_ok")]
+pub struct TopologyOk {
+    #[serde(flatten)]
+    headers: Headers,
+}
+
+#[instrument(skip(state))]
+pub async fn topology(state: State, req: Topology) -> TopologyOk {
+    *state.topology.write().await = req.topology;
+    TopologyOk {
+        headers: req.headers.reply(),
+    }
+}
+
+//                     _
+//  _ __ ___  __ _  __| |
+// | '__/ _ \/ _` |/ _` |
+// | | |  __/ (_| | (_| |
+// |_|  \___|\__,_|\__,_|
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(tag = "type", rename = "read")]
+pub struct Read {
+    #[serde(flatten)]
+    headers: Headers,
+}
+
+impl Read {
+    fn ok(self, messages: Vec<u64>) -> ReadOk {
+        ReadOk {
+            headers: self.headers.reply(),
+            messages,
+        }
+    }
+}
+
+#[derive(Debug, Default, Serialize)]
+#[serde(tag = "type", rename = "read_ok")]
+pub struct ReadOk {
+    #[serde(flatten)]
+    headers: Headers,
+    messages: Vec<u64>,
+}
+
+#[instrument(skip(state))]
+pub async fn read(state: State, req: Read) -> ReadOk {
+    req.ok(state.messages.read().await.clone())
+}
+
+//  _                         _               _
+// | |__  _ __ ___   __ _  __| | ___ __ _ ___| |_
+// | '_ \| '__/ _ \ / _` |/ _` |/ __/ _` / __| __|
+// | |_) | | | (_) | (_| | (_| | (_| (_| \__ \ |_
+// |_.__/|_|  \___/ \__,_|\__,_|\___\__,_|___/\__|
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(tag = "type", rename = "broadcast")]
+pub struct Broadcast {
+    #[serde(flatten)]
+    headers: Headers,
+    message: u64,
+}
+
+impl Broadcast {
+    fn ok(self) -> BroadcastOk {
+        BroadcastOk {
+            headers: self.headers.reply(),
+        }
+    }
+}
+
+#[derive(Debug, Default, Serialize)]
+#[serde(tag = "type", rename = "broadcast_ok")]
+pub struct BroadcastOk {
+    #[serde(flatten)]
+    headers: Headers,
+}
+
 #[instrument(skip(rpc, ids, state))]
 pub async fn broadcast<O>(rpc: Rpc<O>, ids: Ids, state: State, req: Broadcast) -> BroadcastOk
 where
@@ -38,18 +141,12 @@ where
     req.ok()
 }
 
-#[instrument(skip(state))]
-pub async fn read(state: State, req: Read) -> ReadOk {
-    req.ok(state.messages.read().await.clone())
-}
-
-#[instrument(skip(state))]
-pub async fn topology(state: State, req: Topology) -> TopologyOk {
-    *state.topology.write().await = req.topology;
-    TopologyOk {
-        headers: req.headers.reply(),
-    }
-}
+//                 _ _           _
+//  _ __ ___ _ __ | (_) ___ __ _| |_ ___
+// | '__/ _ \ '_ \| | |/ __/ _` | __/ _ \
+// | | |  __/ |_) | | | (_| (_| | ||  __/
+// |_|  \___| .__/|_|_|\___\__,_|\__\___|
+//          |_|
 
 fn replicate_message<O>(rpc: Rpc<O>, ids: Ids, state: State, message: u64)
 where
@@ -98,6 +195,27 @@ where
     }
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(tag = "type", rename = "replicate")]
+pub struct Replicate {
+    msg_id: MsgId,
+    message: u64,
+}
+
+impl Replicate {
+    fn ok(self) -> ReplicateOk {
+        ReplicateOk {
+            in_reply_to: self.msg_id,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "type", rename = "replicate_ok")]
+pub struct ReplicateOk {
+    in_reply_to: MsgId,
+}
+
 #[instrument(skip(state))]
 pub async fn replicate(state: State, req: Replicate) -> ReplicateOk {
     state.messages.write().await.push(req.message);
@@ -132,96 +250,4 @@ pub async fn error(state: State, err: Error) {
             "received error in response to message never sent or no longer pending"
         ),
     }
-}
-
-#[derive(Clone, Default)]
-pub struct State {
-    messages: Arc<RwLock<Vec<u64>>>,
-    topology: Arc<RwLock<HashMap<NodeId, Vec<NodeId>>>>,
-
-    next_id: Arc<AtomicU64>,
-    pending: Arc<RwLock<HashMap<MsgId, Message<Replicate>>>>,
-}
-
-#[derive(Debug, Default, Deserialize)]
-#[serde(tag = "type", rename = "broadcast")]
-pub struct Broadcast {
-    #[serde(flatten)]
-    headers: Headers,
-    message: u64,
-}
-
-impl Broadcast {
-    fn ok(self) -> BroadcastOk {
-        BroadcastOk {
-            headers: self.headers.reply(),
-        }
-    }
-}
-
-#[derive(Debug, Default, Serialize)]
-#[serde(tag = "type", rename = "broadcast_ok")]
-pub struct BroadcastOk {
-    #[serde(flatten)]
-    headers: Headers,
-}
-
-#[derive(Debug, Default, Deserialize)]
-#[serde(tag = "type", rename = "read")]
-pub struct Read {
-    #[serde(flatten)]
-    headers: Headers,
-}
-
-impl Read {
-    fn ok(self, messages: Vec<u64>) -> ReadOk {
-        ReadOk {
-            headers: self.headers.reply(),
-            messages,
-        }
-    }
-}
-
-#[derive(Debug, Default, Serialize)]
-#[serde(tag = "type", rename = "read_ok")]
-pub struct ReadOk {
-    #[serde(flatten)]
-    headers: Headers,
-    messages: Vec<u64>,
-}
-
-#[derive(Debug, Default, Deserialize)]
-#[serde(tag = "type", rename = "topology")]
-pub struct Topology {
-    #[serde(flatten)]
-    headers: Headers,
-    topology: HashMap<NodeId, Vec<NodeId>>,
-}
-
-#[derive(Debug, Default, Serialize)]
-#[serde(tag = "type", rename = "topology_ok")]
-pub struct TopologyOk {
-    #[serde(flatten)]
-    headers: Headers,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(tag = "type", rename = "replicate")]
-pub struct Replicate {
-    msg_id: MsgId,
-    message: u64,
-}
-
-impl Replicate {
-    fn ok(self) -> ReplicateOk {
-        ReplicateOk {
-            in_reply_to: self.msg_id,
-        }
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(tag = "type", rename = "replicate_ok")]
-pub struct ReplicateOk {
-    in_reply_to: MsgId,
 }
